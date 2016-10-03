640414c385dc8c4ccd040a11bfc9b274e28149fa
Philippe Tjon - A - Hen <philippe@tjonahen.nl>
2016-10-03 08:49:15 +0200

    Tests, ecmascript6 syntax

--- main.js
+++ main.js
@@ -14,112 +14,129 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-var http = require("http"),
-     url = require("url"),
-     path = require("path"),
-     port = process.argv[2] || 8181;
+"use strict";
+(() => {
+   var http = require("http"),
+        url = require("url"),
+        path = require("path"),
+        port = process.argv[2] || 8181;
 
-var responseMessage = [];
+   var responseMessage = [];
 
-function endsWith(str, suffix) {
-   return str.indexOf(suffix, str.length - suffix.length) !== -1;
-}
-;
-function unsupportedMediaType(response) {
-   sendHtppResponse(response, 415, "415 Unsupported Media Type\n");
-}
-;
-function methodNotAllowed(response) {
-   sendHtppResponse(response, 405, "405: Methode not allowed\n");
-}
-;
-function sendHtppResponse(response, code, message) {
-   response.writeHead(code, {"Content-Type": "text/plain"});
-   response.write(message);
-   response.end();
-}
+   function endsWith(str, suffix) {
+      return str.indexOf(suffix, str.length - suffix.length) !== -1;
+   }
+   ;
+   function unsupportedMediaType(response) {
+      sendHtppResponse(response, 415, "415 Unsupported Media Type\n");
+   }
+   ;
+   function methodNotAllowed(response) {
+      sendHtppResponse(response, 405, "405: Methode not allowed\n");
+   }
+   ;
+   function sendHtppResponse(response, code, message) {
+      response.writeHead(code, {"Content-Type": "text/plain"});
+      response.write(message);
+      response.end();
+   }
 
-function processSoap(request, response) {
-   if (request.method === 'POST') {
+   function processSoap(request, response) {
+      if (request.method === 'POST') {
+         var body = '';
+
+         request.on('data', (data) => {
+            body += data;
+            if (body.length > 1e6) {
+               request.connection.destroy();
+            }
+         });
+
+         request.on('end', () => {
+            for (i = 0; i < responseMessage.length; i++) {
+               if (responseMessage[i].match.test(body)) {
+                  response.writeHead(200, {"Content-Type": "text/xml"});
+                  response.write(responseMessage[i].data);
+                  response.end();
+                  return;
+               }
+            }
+            response.writeHead(500, {"Content-Type": "text/xml"});
+            response.write("<S:Envelope xmlns:S=\"http://schemas.xmlsoap.org/soap/envelope/\"><S:Body><S:Fault xmlns:ns4=\"http://www.w3.org/2003/05/soap-envelope\"><faultcode>S:Client</faultcode><faultstring>Cannot Consume Message</faultstring></S:Fault></S:Body></S:Envelope>");
+            response.end();
+         });
+      } else {
+         methodNotAllowed(response);
+      }
+   }
+   ;
+
+   function processResponseMessage(request, response) {
       var body = '';
-
-      request.on('data', function (data) {
+      request.on('data', (data) => {
          body += data;
          if (body.length > 1e6) {
             request.connection.destroy();
          }
       });
 
-      request.on('end', function () {
-         for (i = 0; i < responseMessage.length; i++) {
-            if (responseMessage[i].match.test(body)) {
-               response.writeHead(200, {"Content-Type": "text/xml"});
-               response.write(responseMessage[i].data);
-               response.end();
-               return;
-            }
+      request.on('end', () => {
+         newMessageResponse = JSON.parse(body);
+         if (newMessageResponse.match !== undefined) {
+            newMessageResponse.match = new RegExp(newMessageResponse.match, "m");
+            responseMessage.push(newMessageResponse);
+            sendHtppResponse(response, 202, "202: accepted\n");
+         } else {
+            sendHtppResponse(response, 400, "400 Bad Request\n");
          }
-         response.writeHead(500, {"Content-Type": "text/xml"});
-         response.write("<S:Envelope xmlns:S=\"http://schemas.xmlsoap.org/soap/envelope/\"><S:Body><S:Fault xmlns:ns4=\"http://www.w3.org/2003/05/soap-envelope\"><faultcode>S:Client</faultcode><faultstring>Cannot Consume Message</faultstring></S:Fault></S:Body></S:Envelope>");
-         response.end();
       });
-   } else {
-      methodNotAllowed(response);
    }
-}
-;
+   ;
 
-function processResponseMessage(request, response) {
-   var body = '';
-   request.on('data', function (data) {
-      body += data;
-      if (body.length > 1e6) {
-         request.connection.destroy();
+   function processSetResponseMessage(request, response) {
+      if (request.method === 'POST') {
+         processResponseMessage(request, response);
+      } else if (request.method === 'PUT') {
+         responseMessage = [];
+         processResponseMessage(request, response);
+      } else {
+         methodNotAllowed(response);
+      }
+   }
+   ;
+
+   var server = http.createServer((request, response) => {
+      var uri = url.parse(request.url).pathname,
+           filename = path.join(process.cwd(), decodeURI(uri));
+      if (endsWith(filename, 'api/endpoint')) {
+         if (request.headers['content-type'] === 'text/xml') {
+            processSoap(request, response);
+         } else {
+            unsupportedMediaType(response);
+         }
+      } else if (endsWith(filename, 'api/data')) {
+         if (request.headers['content-type'] === 'application/json') {
+            processSetResponseMessage(request, response);
+         } else {
+            unsupportedMediaType(response);
+         }
+      } else {
+         sendHtppResponse(response, 404, "404 Not Found\n");
       }
    });
 
-   request.on('end', function () {
-      newMessageResponse = JSON.parse(body);
-      if (newMessageResponse.match !== undefined) {
-         newMessageResponse.match = new RegExp(newMessageResponse.match, "m");
-         responseMessage.push(newMessageResponse);
-         sendHtppResponse(response, 202, "202: accepted\n");
-      } else {
-         sendHtppResponse(response, 400, "400 Bad Request\n");
+   server.on('error', (e) => {
+      if (e.code === 'EADDRINUSE') {
+         console.log('Address in use, retrying...');
+         setTimeout(() => {
+            server.close();
+            server.listen(parseInt(port, 10));
+         }, 1000);
       }
    });
-};
 
-function processSetResponseMessage(request, response) {
-   if (request.method === 'POST') {
-      processResponseMessage(request, response);
-   } else if (request.method === 'PUT') {
-      responseMessage = [];
-      processResponseMessage(request, response);
-   } else {
-      methodNotAllowed(response);
-   }
-};
+   server.listen(parseInt(port, 10));
 
-http.createServer(function (request, response) {
-   var uri = url.parse(request.url).pathname,
-        filename = path.join(process.cwd(), decodeURI(uri));
-   var headers = request.headers;
-   if (endsWith(filename, 'api/endpoint')) {
-      if (headers['Content-Type'] === 'text/xml' || headers['content-type'] === 'text/xml') {
-         processSoap(request, response);
-      } else {
-         unsupportedMediaType(response);
-      }
-   } else if (endsWith(filename, 'api/data')) {
-      if (headers['Content-Type'] === 'application/json' || headers['content-type'] === 'application/json') {
-         processSetResponseMessage(request, response);
-      } else {
-         unsupportedMediaType(response);
-      }
-   } else {
-      sendHtppResponse(response, 404, "404 Not Found\n");
-   }
-}).listen(parseInt(port, 10));
+   console.log("SOAP Stub endpoint running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
 
-console.log("SOAP Stub endpoint running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
+})();
